9aecac3e1d77ded98f4535f6d9a106d83d26401d
Manning Chen <manningchen33@gmail.com>
2024-04-07 05:58:34 -0400

    lualine config initial

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,11 +1,16 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    require("lualine").setup({
-      options = {
-        icons_enabled = true,
-        theme = "catppuccin",
-      },
-    })
-  end,
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+				theme = "catppuccin",
+				ignore_focus = { "NvimTree" },
+			},
+			sections = {
+				lualine_b = { "branch" },
+				lualine_c = { { "filename", path = 1 } },
+			},
+		})
+	end,
 }
