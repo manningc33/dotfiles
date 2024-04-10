7e711c4333884b7d97f873594e00b5e1b6221789
Manning Chen <manningchen33@gmail.com>
2024-04-10 00:19:54 -0400

    fixed some autocomplete features

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -6,6 +6,7 @@ return {
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-buffer", --  source for text in buffer
 			"hrsh7th/cmp-path", -- source for file system paths in commands
+			"hrsh7th/cmp-cmdline", -- source command line
 			"L3MON4D3/LuaSnip", -- snippet engine
 			"saadparwaiz1/cmp_luasnip", -- for lua autocompletion
 			"rafamadriz/friendly-snippets", -- snippets library
@@ -18,16 +19,12 @@ return {
 
 			cmp.setup({
 				completion = {
-					completeopt = "menu,menuone,preview",
+					completeopt = "menu,menuone",
 				},
 				snippet = {
 					expand = function(args)
 						require("luasnip").lsp_expand(args.body)
 					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
 				},
 				mapping = cmp.mapping.preset.insert({
 					["<PageUp>"] = cmp.mapping.scroll_docs(-4),
@@ -43,6 +40,34 @@ return {
 					{ name = "buffer" },
 					{ name = "path" },
 				}),
+			})
+
+			-- Set configuration for specific filetype.
+			cmp.setup.filetype("gitcommit", {
+				sources = cmp.config.sources({
+					{ name = "git" },
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+
+			-- Use buffer source for `/` and `?`
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+
+			-- Use cmdline & path source for ':'
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline" },
+				}),
+				matching = { disallow_symbol_nonprefix_matching = false },
 			})
 		end,
 	},
--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -33,9 +33,9 @@ return {
 			})
 			require("mason-lspconfig").setup_handlers({
 				function(server_name) -- default handler (optional)
-					local capabilities = require("cmp_nvim_lsp").default_capabilities()
+					--local capabilities = require("cmp_nvim_lsp").default_capabilities()
 					require("lspconfig")[server_name].setup({
-						capabilities = capabilities,
+						--	capabilities = capabilities,
 					})
 				end,
 				["lua_ls"] = function()
