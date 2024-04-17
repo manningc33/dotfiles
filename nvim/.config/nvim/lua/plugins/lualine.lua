5323ef2fe51723f293644909761e711bd6214864
Manning Chen <manningchen33@gmail.com>
2024-04-17 16:06:49 -0500

    customize lualine tabline and winbar
    nvimtree close on open

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -5,12 +5,28 @@ return {
       options = {
         icons_enabled = true,
         theme = "catppuccin",
+        disabled_filetypes = {
+          statusline = { "NvimTree" },
+          winbar = { "NvimTree" },
+        },
         ignore_focus = { "NvimTree" },
       },
       sections = {
-        lualine_b = { "" },
-        lualine_c = { { "filename", path = 1 } },
+        lualine_b = { "diff", "diagnostics" },
+        lualine_c = { { "filename", newfile_status = true } },
+        lualine_x = { "fileformat", "filetype" },
       },
+      tabline = {
+        lualine_b = { { 'branch', color = { fg = "#b4befe" } } },
+        lualine_c = { { 'buffers', mode = 2 } },
+      },
+      winbar = {
+        lualine_c = { { 'filename', path = 1, newfile_status = true, } },
+        lualine_x = { "searchcount" }
+      },
+      inactive_winbar = {
+        lualine_c = { { 'filename', path = 1, newfile_status = true } },
+      }
     })
   end,
 }
