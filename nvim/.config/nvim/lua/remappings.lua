2ff9af5b8e9036eba2600e5689e49aaf29efb724
Manning Chen <manningchen33@gmail.com>
2024-04-17 12:00:41 -0500

    no branch on lualine, no switch line in insert mode, edited python linter

--- nvim/.config/nvim/lua/remappings.lua
+++ nvim/.config/nvim/lua/remappings.lua
@@ -20,18 +20,18 @@ vim.keymap.set("n", "k", [[ v:count > 1 ? "m'" . v:count . "k" : "gk" ]], { expr
 
 -- smart beginning and end of line
 keymap.set({ "n", "v", "i" }, "<Home>", function()
-	local column = vim.fn.col(".")
-	vim.cmd("normal! ^")
-	if column == vim.fn.col(".") then
-		vim.cmd("normal! 0")
-	end
+  local column = vim.fn.col(".")
+  vim.cmd("normal! ^")
+  if column == vim.fn.col(".") then
+    vim.cmd("normal! 0")
+  end
 end, { silent = true, desc = "go to start of line" })
 keymap.set("o", "<Home>", "^", { desc = "go to start of line" })
 
 -- better paste
 vim.keymap.set("n", "<leader>p", function()
-	vim.fn.setreg("+", vim.fn.getreg("+"), "V")
-	vim.cmd("normal p")
+  vim.fn.setreg("+", vim.fn.getreg("+"), "V")
+  vim.cmd("normal p")
 end, { desc = "paste as lines" })
 
 -- jump back to last position
@@ -40,35 +40,35 @@ end, { desc = "paste as lines" })
 vim.keymap.set("n", "gl", "`.", { desc = "goto to last change" })
 
 -- move visual blocks
-vim.keymap.set({ "n", "i" }, "<M-Up>", "<cmd>m -2<CR>", { silent = true, desc = "move block up" })
-vim.keymap.set({ "n", "i" }, "<M-Down>", "<cmd>m +1<CR>", { silent = true, desc = "move block up" })
+vim.keymap.set("n", "<M-Up>", "<cmd>m -2<CR>", { silent = true, desc = "move block up" })
+vim.keymap.set("n", "<M-Down>", "<cmd>m +1<CR>", { silent = true, desc = "move block up" })
 vim.keymap.set("v", "<M-Up>", ":m '<-2<CR>gv=gv", { silent = true, desc = "move block up" })
 vim.keymap.set("v", "<M-Down>", ":m '>+1<CR>gv=gv", { silent = true, desc = "move block down" })
 
 -- movement between buffers
 vim.keymap.set("n", "<tab>", function()
-	vim.cmd("bn")
+  vim.cmd("bn")
 end, { silent = true, desc = "go to next buffer" })
 vim.keymap.set("n", "<s-tab>", function()
-	vim.cmd("bp")
+  vim.cmd("bp")
 end, { silent = true, desc = "go to previous buffer" })
 
 -- indent on insert in empty lines
 vim.keymap.set("n", "i", function()
-	if #vim.fn.getline(".") == 0 then
-		return [["_cc]]
-	else
-		return "i"
-	end
+  if #vim.fn.getline(".") == 0 then
+    return [["_cc]]
+  else
+    return "i"
+  end
 end, { desc = "enter insert mode", expr = true })
 
 -- no yank delete
 vim.keymap.set("n", ")", '"_d', { desc = "delete without yanking" })
 vim.keymap.set("n", "))", '"_dd', { desc = "delete line without yanking" })
 vim.keymap.set("n", "dd", function()
-	if vim.api.nvim_get_current_line():match("^%s*$") then
-		return '"_dd'
-	else
-		return "dd"
-	end
+  if vim.api.nvim_get_current_line():match("^%s*$") then
+    return '"_dd'
+  else
+    return "dd"
+  end
 end, { desc = "delete line", expr = true })
