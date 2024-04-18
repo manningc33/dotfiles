b4c84d797e2cab06827e75df05d12556e251f06d
Manning Chen <manningchen33@gmail.com>
2024-04-18 10:51:32 -0500

    autopairs
    autoclose "([{}])"

---
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,20 @@
+return {
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/nvim-cmp",
+  },
+  config = function()
+    local autopairs = require("nvim-autopairs")
+    autopairs.setup({
+      check_ts = true,
+      ts_config = {
+        lua = { "string" },
+      }
+    })
+    -- If you want insert `(` after select function or method item
+    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+    local cmp = require('cmp')
+    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+  end,
+}
