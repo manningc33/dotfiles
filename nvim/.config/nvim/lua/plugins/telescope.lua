2fd2d12ecad455fe77182c1e5a1efb34137e0db4
Manning Chen <Manning.Chen@plexus.com>
2024-04-23 15:44:45 -0500

    added telescope undo

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,20 +2,21 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.6",
-    dependencies = { { "nvim-lua/plenary.nvim" } },
+    dependencies = { { "nvim-lua/plenary.nvim", "debugloop/telescope-undo.nvim", } },
     config = function()
       local builtin = require("telescope.builtin")
       vim.keymap.set(
         "n",
         "\\",
         "<cmd>lua require'telescope.builtin'.find_files({find_command = { 'rg', '--files', '--hidden', '-g', '!.git' }})<CR>",
-        {desc = "file selector"}
+        { desc = "file selector" }
       )
       vim.keymap.set("n", "<leader>f", builtin.live_grep, { desc = "find in all files" })
       vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "buffers selector" })
 
       local actions = require("telescope.actions")
-      require("telescope").setup({
+      local telescope = require("telescope")
+      telescope.setup({
         defaults = {
           mappings = {
             i = {
@@ -32,7 +33,25 @@ return {
             },
           },
         },
+        extensions = {
+          undo = {
+            side_by_side = true,
+            layout_strategy = "vertical",
+            layout_config = {
+              preview_height = 0.8,
+            },
+            mappings = {
+              i = {
+                ["<cr>"] = require("telescope-undo.actions").restore,
+                ["<S-cr>"] = require("telescope-undo.actions").yank_deletions,
+                ["<C-cr>"] = require("telescope-undo.actions").yank_additions,
+              }
+            }
+          }
+        }
       })
+      telescope.load_extension("undo")
+      vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>")
     end,
   },
 }
