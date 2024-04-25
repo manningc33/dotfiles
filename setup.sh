3b9aa62ad489a7eb96e196d0678fd5be79cc413b
Manning Chen <manningchen33@gmail.com>
2024-04-25 11:53:12 -0500

    update setup to install stow and wget

--- setup.sh
+++ setup.sh
@@ -5,61 +5,68 @@ dotfiles_dir=~/dotfiles
 git pull origin master
 
 function homebrewInstall() {
-  ! command -v brew &>/dev/null && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  brew update
-  brew upgrade
+	! command -v brew &>/dev/null && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	brew update
+	brew upgrade
 
-  pkgs=(
-    bat
-    eza 
-    fzf 
-    git
-    git-delta
-    lazygit
-    neofetch
-    neovim
-    ripgrep
-    starship
-    tmux 
-    zoxide
-  )
+	pkgs=(
+		bat
+		eza
+		fzf
+		git
+		git-delta
+		lazygit
+		neofetch
+		neovim
+		ripgrep
+		starship
+		stow
+		tmux
+		wget
+		zoxide
+	)
 
-  brew install "${pkgs[@]}"
+	brew install "${pkgs[@]}"
 }
 
 function catppuccinThemesInstall() {
 	# get bat catppuccin theme
-  echo "Getting bat and delta catppuccin themes if not present..."
+	echo "Getting bat and delta catppuccin themes if not present..."
 	[ ! -f ~/.config/bat/themes/Catppuccin\ Mocha.tmTheme ] && wget -P ~/.config/bat/themes https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme
 	[ ! -f ~/.local/share/catppuccin/delta/catppuccin.gitconfig ] && wget -P ~/.local/share/catppuccin/delta https://github.com/catppuccin/delta/raw/main/catppuccin.gitconfig
 }
 
-function createLinks() {
+function stowCreateLinks() {
 	mkdir -p "$HOME/.config"
 	# stowing all modules
-	stow bash
-	stow git
-	stow nvim
-	stow starship
-	stow tmux
+	stow_modules=(
+		bash
+		git
+		nvim
+		starship
+		tmux
+		# other
+	)
 
-  . ~/.profile
+	stow "${stow_modules[@]}"
+
+	. ~/.profile
 	. ~/.bashrc
 }
 
 function runSetup() {
-  homebrewInstall
-  catppuccinThemesInstall
-  createLinks
+	homebrewInstall
+	catppuccinThemesInstall
+	stowCreateLinks
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-  runSetup
+	runSetup
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 	echo ""
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-    runSetup
+		runSetup
 	fi
 fi
 
@@ -68,4 +75,4 @@ echo "Dotfiles setup finish"
 unset runSetup
 unset homebrewInstall
 unset catppuccinThemesInstall
-unset createLinks
+unset stowCreateLinks
